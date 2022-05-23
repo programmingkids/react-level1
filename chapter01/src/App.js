3fbada4330f04571f0fb9fb70aadbeb6d36112b8
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-05-23 11:48:15 +0000

    added chapter01

---
+++ chapter01/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello, React</h1>
+        <h2>This is Raect app</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
