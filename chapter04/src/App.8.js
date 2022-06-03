ec8d92c0a5837919b50be1820e7dee97bb7afe53
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-06-03 10:37:47 +0000

    added work08

--- chapter04/src/App.8.js
+++ chapter04/src/App.8.js
@@ -8,92 +8,24 @@ function getRandomNumber(min, max) {
 }
 
 function App() {
-  const kujiData = ['大吉','中吉','小吉','吉','凶'];
-  const resultData = kujiData.map(name => ({name, count:0, ratio:0}));
-  
-  const [omikuji, setOmikuji] = useState({
-    kuji : '',
-    total : 0,
-    resultList : resultData,
-  });
+  const [message, setMessage] = useState('How are you?');
   
   const handleClick = (event) => {
-    const min = 0;
-    const max = kujiData.length - 1;
-    const randomNum = getRandomNumber(min, max);
-    
-    const kuji = kujiData[randomNum];
-    
-    setOmikuji(omikuji => {
-      let { total, resultList} = omikuji;
-      total++;
-      resultList = resultList.map(row => {
-        if(row.name === kuji) {
-          row.count++;
-        }
-        row.ratio = Math.round(row.count / total * 100);
-        return row;
-      });
-      return {kuji, total, resultList};
-    });
+    setMessage(message => 'Great!');
   };
   
   return (
     <div className="App">
-      <Title>おみくじ</Title>
+      <Title>メッセージ</Title>
       <div>
-        <button onClick={handleClick}>くじを見る</button>
+        <button onClick={handleClick}>変更</button>
       </div>
-      <OmikujiBox kuji={omikuji.kuji} total={omikuji.total} />
       <hr />
-      <h2>結果一覧</h2>
-      <ResultBox resultList={omikuji.resultList} />
+      <div>{message}</div>
     </div>
   );
 }
 
 const Title = (props) => <h1>{props.children}</h1>;
-
-const OmikujiBox = (props) => (
-  <div>
-    <div>今日の運勢：{props.kuji}</div>
-    <div>合計回数：{props.total} 回</div>
-  </div>
-);
-
-const ResultBox = (props) => (
-  <table className="my-table">
-    <ResultHeader />
-    <ResultBody resultList={props.resultList} />
-  </table>
-);
-
-const ResultHeader = () => (
-  <thead>
-    <tr>
-      <th>結果</th>
-      <th>回数</th>
-      <th>確率</th>
-    </tr>
-  </thead>
-);
-
-const ResultBody = (props) => (
-  <tbody>
-  {props.resultList.map(value => <ResultItem key={value.name} data={value} />)}
-  </tbody>
-);
-
-const ResultItem = (props) => {
-  const {name, count, ratio} = props.data;
-  
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{count}</td>
-      <td>{ratio}%</td>
-    </tr>
-  );
-};
 
 export default React.memo(App);
--- chapter04/src/App.js
+++ chapter04/src/App.js
@@ -8,89 +8,24 @@ function getRandomNumber(min, max) {
 }
 
 function App() {
-  const kuji = ['大吉','中吉','小吉','吉','凶'];
-  const resultData = kuji.map(name => ({name, count:0, ratio:0}));
-  
-  let [omikuji, setOmikuji] = useState('');
-  let [total, setTotal] = useState(0);
-  const [resultList, setResultList] = useState(resultData);
+  const [message, setMessage] = useState('How are you?');
   
   const handleClick = (event) => {
-    const min = 0;
-    const max = kuji.length - 1;
-    const randomNum = getRandomNumber(min, max);
-    
-    omikuji = kuji[randomNum];
-    setOmikuji(omikuji);
-    
-    total++;
-    setTotal(total);
-    
-    setResultList(resultList.map(row => {
-      if(row.name === omikuji) {
-        row.count++;
-      }
-      row.ratio = Math.round(row.count / total * 100);
-      return row;
-    }));
+    setMessage(message => 'Great!');
   };
   
   return (
     <div className="App">
-      <Title>おみくじ</Title>
+      <Title>メッセージ</Title>
       <div>
-        <button onClick={handleClick}>くじを見る</button>
+        <button onClick={handleClick}>変更</button>
       </div>
-      <OmikujiBox omikuji={omikuji} total={total} />
       <hr />
-      <h2>結果一覧</h2>
-      <ResultBox data={resultList} />
+      <div>{message}</div>
     </div>
   );
 }
 
 const Title = (props) => <h1>{props.children}</h1>;
-
-const OmikujiBox = (props) => (
-  <div>
-    <div>今日の運勢：{props.omikuji}</div>
-    <div>合計回数：{props.total} 回</div>
-  </div>
-);
-
-const ResultBox = (props) => (
-  <table className="my-table">
-    <ResultHeader />
-    <ResultBody data={props.data} />
-  </table>
-);
-
-const ResultHeader = () => (
-  <thead>
-    <tr>
-      <th>結果</th>
-      <th>回数</th>
-      <th>確率</th>
-    </tr>
-  </thead>
-);
-
-const ResultBody = (props) => (
-  <tbody>
-  {props.data.map(value => <ResultItem key={value.name} data={value} />)}
-  </tbody>
-);
-
-const ResultItem = (props) => {
-  const {name, count, ratio} = props.data;
-  
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{count}</td>
-      <td>{ratio}%</td>
-    </tr>
-  );
-};
 
 export default React.memo(App);
