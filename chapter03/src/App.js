e695433ed1b02f3dbb134ec44e2dc0270cceefbc
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-07-13 04:29:39 +0000

    fix chapter03 for main

--- chapter03/src/App.js
+++ chapter03/src/App.js
@@ -2,75 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const scores = [{
-    name : "Donald",
-    ja : 85,
-    ma : 92,
-    en : 90,
-  },{
-    name : "Anna",
-    ja : 95,
-    ma : 85,
-    en : 82,
-  },{
-    name : "Elsa",
-    ja : 88,
-    ma : 94,
-    en : 93,
-  },{
-    name : "Ariel",
-    ja : 90,
-    ma : 82,
-    en : 85,
-  }];
-  
   return (
     <div className="App">
-      <Title>スコア一覧</Title>
-      <ScoreTable data={scores} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
-const Title = (props) => <h1>{props.children}</h1>;
-
-const ScoreTable = (props) => (
-  <table className="score-table">
-    <ScoreHeader />
-    <ScoreBody data={props.data} />
-  </table>
-);
-
-const ScoreHeader = () => (
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Japanese</th>
-      <th>Math</th>
-      <th>English</th>
-      <th>Total</th>
-    </tr>
-  </thead>
-);
-
-const ScoreBody = (props) => (
-  <tbody>
-    {props.data.map((value) => <Score key={value.name} data={value} />)}
-  </tbody>
-);
-
-const Score = (props) => {
-  const {name, ja, ma, en} = props.data;
-  const total = ja + ma + en;
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{ja}</td>
-      <td>{ma}</td>
-      <td>{en}</td>
-      <td>{total}</td>
-    </tr>
-  );
-};
 
 export default App;
