e4e1ed2723dc13554a13ab394e0b742b574b79af
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-07-20 09:55:52 +0000

    fix chapter04 for main

--- chapter04/src/App.js
+++ chapter04/src/App.js
@@ -1,53 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
-
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 function App() {
-  const [number, setNumber] = useState({
-    result : '',
-    resultList : [],
-  });
-  
-  const handleClick = (event) => {
-    const min = 10000;
-    const max = 99999;
-    const result = getRandomNumber(min,max);
-    const resultList = number.resultList.concat([result]);
-    
-    setNumber(number => ({result, resultList}));
-  };
-  
   return (
     <div className="App">
-      <Title>ランダムな数値</Title>
-      <div>
-        <button onClick={handleClick}>数値を見る</button>
-      </div>
-      <NumberBox result={number.result} />
-      <hr />
-      <ResultBox resultList={number.resultList} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
-const NumberBox = (props) => (
-  <div>ランダムな数値：{props.result}</div>
-);
-
-const ResultBox =(props) => (
-  <div className="my-list">
-  {props.resultList.map((value, index) => <ResultItem key={index} value={value} />)}
-  </div>
-);
-
-const ResultItem = (props) => (
-  <li>{props.value}</li>  
-);
-
-const Title = (props) => <h1>{props.children}</h1>;
 
 export default App;
